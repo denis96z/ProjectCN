a127450d996cf098814b29443569123e8c16b9b8
denis96z <zinoviev.denis@yandex.ru>
2017-11-30 23:26:03 +0300

    Added FileManager

---
+++ FileTransferManager/FileTransferManagerServer/FileManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransferManager
+{
+    class FileManager : IFileManager
+    {
+        public bool DeleteFile(string path)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RecvFile(string path, Socket socket, long fileSize)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SendFile(string path, Socket socket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FileTransferManager/FileTransferManagerServer/IFileManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileTransferManager
+{
+    interface IFileManager
+    {
+        bool RecvFile(string path, Socket socket, long fileSize);
+        bool SendFile(string path, Socket socket);
+        bool DeleteFile(string path);
+    }
+}
