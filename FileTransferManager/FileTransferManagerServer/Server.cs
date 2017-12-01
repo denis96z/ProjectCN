7ed6d72a34f48f524e6049fa8e8f4fcaaaa1ad8c
denis96z <zinoviev.denis@yandex.ru>
2017-12-01 04:36:45 +0300

    Added server class.

---
+++ FileTransferManager/FileTransferManagerServer/Server.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using ServerAddress = FileTransferManager.ServerAddress;
+
+namespace FileTransferManagerServer
+{
+    class Server
+    {
+        private TcpListener tcpListener =
+            new TcpListener(IPAddress.Any, ServerAddress.SERVER_PORT);
+
+        public void StartServer()
+        {
+            new Thread(() =>
+            {
+                try
+                {
+                    tcpListener.Start();
+
+                    while (true)
+                    {
+                        var client = tcpListener.AcceptTcpClient();
+                        new Thread(() => new UserManager(client.Client)
+                            .HandleRequests()).Start();
+                    }
+                }
+                catch
+                {
+                    
+                }
+            }).Start();
+        }
+
+        public void StopServer()
+        {
+            tcpListener.Stop();
+        }
+    }
+}
---
+++ FileTransferManager/FileTransferManagerServer/ServerAddress.cs
@@ -0,0 +1,8 @@
+﻿namespace FileTransferManager
+{
+    class ServerAddress
+    {
+        public const string SERVER_DNS = "DENIS-PK";
+        public const int SERVER_PORT = 30300;
+    }
+}
