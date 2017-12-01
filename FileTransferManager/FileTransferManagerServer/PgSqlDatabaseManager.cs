103e7cc42f9ac2a663afb16bbbbe3f5bd8152d83
denis96z <zinoviev.denis@yandex.ru>
2017-12-01 03:25:51 +0300

    Implemented DatabaseManager.

--- FileTransferManager/FileTransferManagerServer/PgSqlDatabaseManager.cs
+++ FileTransferManager/FileTransferManagerServer/PgSqlDatabaseManager.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Text;
 using Devart.Data.PostgreSql;
 
 namespace FileTransferManagerServer
@@ -10,61 +10,127 @@ namespace FileTransferManagerServer
 
         public void Register(string login, string password)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("Register", login, password);
+            ExecuteNonQuery(commandText);
         }
 
         public void SignIn(string login, string password)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("SignIn", login, password);
+            ExecuteNonQuery(commandText);
         }
 
         public void SignOut(string login, string password)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("SignOut", login, password);
+            ExecuteNonQuery(commandText);
         }
 
         public void AddFile(string login, string password,
             string rootDir, string path, long size)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("AddFile",
+                login, password, rootDir, path, size);
+            ExecuteNonQuery(commandText);
         }
 
         public void DeleteFile(string login, string password,
             string path, string owner)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("DeleteFile",
+                login, password, path, owner);
+            ExecuteNonQuery(commandText);
         }
 
         public void ShareFile(string login, string password,
             string path, string user)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("ShareFile",
+                login, password, path, user);
+            ExecuteNonQuery(commandText);
         }
 
         public string GetVirtualFileName(string login,
             string password, string path, string owner)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("GetVirtualFileName",
+                login, password, path, owner);
+            return ExecuteReturnString(commandText);
         }
 
         public string GetUsersList(string login, string password)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("GetUsersList", login, password);
+            return ExecuteReturnList(commandText, 2);
         }
 
         public string GetFilesList(string login, string password)
         {
-            throw new NotImplementedException();
+            string commandText = CreateCommand("GetFilesList", login, password);
+            return ExecuteReturnList(commandText, 4);
         }
 
-        private PgSqlCommand ParseCommand(string command)
+        private string CreateCommand(string function, params object[] args)
         {
-            throw new NotImplementedException();
+            string command = "select * from \"" + function + "\"(";
+            if (args.Length > 0)
+            {
+                int lastIndex = args.Length - 1;
+                for (int i = 0; i < lastIndex; i++)
+                {
+                    command += CreateArgument(args[i]) + ", ";
+                }
+                command += CreateArgument(args[lastIndex]);
+            }
+            command += ");";
+            return command;
         }
 
-        private void ExecuteNonQuery(string command)
+        private string CreateArgument(object arg)
         {
-            throw new NotImplementedException();
+            return arg is string ? "'" + arg.ToString() + "'" : arg.ToString();
+        }
+
+        private void ExecuteNonQuery(string commandText)
+        {
+            PgSqlCommand command = new PgSqlCommand(commandText, connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+
+        private string ExecuteReturnString(string commandText)
+        {
+            PgSqlCommand command = new PgSqlCommand(commandText, connection);
+            connection.Open();
+            var reader = command.ExecuteReader();
+            reader.Read();
+            string result = reader.GetString(0);
+            reader.Close();
+            connection.Close();
+            return result;
+        }
+
+        private string ExecuteReturnList(string commandText, int numColumns)
+        {
+            PgSqlCommand command = new PgSqlCommand(commandText, connection);
+            connection.Open();
+
+            StringBuilder result = new StringBuilder();
+            using (var reader = command.ExecuteReader())
+            {
+                while (reader.Read())
+                {
+                    for (int i = 0; i < numColumns; i++)
+                    {
+                        result.Append(" " + reader.GetValue(i).ToString());
+                    }
+                }
+                reader.Close();
+            }                
+
+            connection.Close();
+            return result.ToString();
         }
     }
 }
