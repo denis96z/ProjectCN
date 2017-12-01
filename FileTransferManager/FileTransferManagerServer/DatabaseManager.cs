a076933cff3ed31237849f97bf52c14ee5fd18e0
denis96z <zinoviev.denis@yandex.ru>
2017-12-01 03:00:26 +0300

    Added DatabaseManager.

---
+++ FileTransferManager/FileTransferManagerServer/DatabaseManager.cs
@@ -0,0 +1,18 @@
+﻿namespace FileTransferManagerServer
+{
+    abstract class DatabaseManager
+    {
+        private const string USER_ID = "postgres";
+        private const string PASSWORD = "0000";
+        private const string HOST = "localhost";
+        private const string PORT = "5432";
+        private const string DATABASE = "FileTransfer";
+
+        protected const string CONNECTION_STRING =
+            "User Id=" + USER_ID + ";" +
+            "Password=" + PASSWORD + ";" +
+            "Host=" + HOST + ";" +
+            "Port=" + PORT + ";" +
+            "Database=" + DATABASE + ";";
+    }
+}
---
+++ FileTransferManager/FileTransferManagerServer/PgSqlDatabaseManager.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using Devart.Data.PostgreSql;
+
+namespace FileTransferManagerServer
+{
+    class PgSqlDatabaseManager : DatabaseManager
+    {
+        private PgSqlConnection connection =
+            new PgSqlConnection(CONNECTION_STRING);
+
+        public void Register(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SignIn(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SignOut(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddFile(string login, string password,
+            string rootDir, string path, long size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteFile(string login, string password,
+            string path, string owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShareFile(string login, string password,
+            string path, string user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetVirtualFileName(string login,
+            string password, string path, string owner)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetUsersList(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetFilesList(string login, string password)
+        {
+            throw new NotImplementedException();
+        }
+
+        private PgSqlCommand ParseCommand(string command)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ExecuteNonQuery(string command)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
